fd9cc78e5c665211871a55b0ac625b7d7ec18197
Ryo Yonetani <ryo.yonetani@sinicx.com>
2022-02-15 06:19:25 -0500

    improve CTRMSampler

--- scripts/eval_example.sh
+++ scripts/eval_example.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # test
-python scripts/eval.py dataset=eval_example sampler=ctrm sampler.num_samples=25
 python scripts/eval.py dataset=eval_example sampler=ctrm sampler.num_samples=50
 python scripts/eval.py dataset=eval_example sampler=ctrm sampler.num_samples=100
+python scripts/eval.py dataset=eval_example sampler=ctrm sampler.num_samples=150
 python scripts/eval.py dataset=eval_example sampler=random_shared sampler.num_samples=3000
 python scripts/eval.py dataset=eval_example sampler=random_shared sampler.num_samples=5000
 python scripts/eval.py dataset=eval_example sampler=random_shared sampler.num_samples=7000
