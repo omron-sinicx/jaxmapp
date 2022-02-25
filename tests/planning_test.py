3c11c5b0865bdc29a89186a79b55649a483da68f
Ryo Yonetani <ryo.yonetani@sinicx.com>
2022-02-25 13:13:28 +0900

    fix planning test

--- tests/planning_test.py
+++ tests/planning_test.py
@@ -8,24 +8,25 @@ def setup():
 
     key = jax.random.PRNGKey(0)
     generator = InstanceGeneratorCircleObs(
-        num_agents_min=11,
-        num_agents_max=20,
-        max_speeds_cands=[0.03125, 0.0390625, 0.046875],
-        rads_cands=[0.015625, 0.01953125, 0.0234375],
+        num_agents_min=5,
+        num_agents_max=5,
+        max_speeds_cands=[0.25],
+        rads_cands=[0.01],
         map_size=160,
-        num_obs=10,
+        num_obs=0,
     )
     ins = generator.generate(key)
     return key, ins
 
 
-def pp(setup):
+def test_pp(setup):
     from jaxmapp.planner.prioritized_planning import PrioritizedPlanning as PP
     from jaxmapp.roadmap.random_sampler import RandomSampler
 
     key, ins = setup
 
-    sampler = RandomSampler(share_roadmaps=True, num_samples=100, max_T=64)
+    sampler = RandomSampler(share_roadmap=True, num_samples=100, max_T=64)
     trms = sampler.construct_trms(key, ins)
     planner = PP(verbose=1)
     res = planner.solve(ins.to_numpy(), trms)
+    print(f"{res=}")
